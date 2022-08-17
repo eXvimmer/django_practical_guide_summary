06a9b1c2c55b53c5e76ff70e6aa2fca4013abeef
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-08-17 14:43:00 +0430

    Change the MeetUp model to have a image field, install Pillow and prepare for image upload.

--- meetups/models.py
+++ meetups/models.py
@@ -5,6 +5,7 @@ class Meetup(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField(unique=True, allow_unicode=True)
     description = models.TextField()
+    image = models.ImageField(upload_to="images")  # /uploads/images
 
     def __str__(self):
         return self.title
