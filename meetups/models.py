293be79b4ee22c24b99fc8222d8a544b2c0ae877
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-08-17 14:09:27 +0430

    Create Meetup model and migrate.

--- meetups/models.py
+++ meetups/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Meetup(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True, allow_unicode=True)
+    description = models.TextField()
