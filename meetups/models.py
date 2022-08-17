5327532eaef3d9181acc68b14ba24f32829824e9
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-08-17 14:14:51 +0430

    Create a superuser, and add Meetup model to admin panel.

--- meetups/models.py
+++ meetups/models.py
@@ -5,3 +5,6 @@ class Meetup(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField(unique=True, allow_unicode=True)
     description = models.TextField()
+
+    def __str__(self):
+        return self.title
