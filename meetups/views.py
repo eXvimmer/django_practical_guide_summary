668dbf3be7db3f30198f5af96574586bf4443aa2
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-08-17 11:30:39 +0430

    Load all meetups and style them.

--- meetups/views.py
+++ meetups/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpRequest
 from django.shortcuts import render
 
-meetups = [{"title": "First Meetup"}, {"title": "Second Meetup"}]
+meetups = [
+    {"title": "First Meetup", "location": "New York", "slug": "first-meetup"},
+    {"title": "Second Meetup", "location": "London", "slug": "second-meetup"},
+]
 
 
 def index(request: HttpRequest):
