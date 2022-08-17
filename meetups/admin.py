ad76d9ea184a2fb6d68101e9b72d2a654e074fd9
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-08-17 17:59:42 +0430

    Create the Location Model, create a many-to-one relationship between Meetup and Location, add it to the admin panel and migrate models.

--- meetups/admin.py
+++ meetups/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from .models import Meetup
+from .models import Location, Meetup
 
 
 class MeetupAdmin(admin.ModelAdmin):
     list_display = ("title", "slug")
-    list_filter = ("title",)  # TODO: filter by location
+    list_filter = ("location",)
     prepopulated_fields = {"slug": ("title",)}
 
 
 admin.site.register(Meetup, MeetupAdmin)
+admin.site.register(Location)
--- meetups/models.py
+++ meetups/models.py
@@ -1,12 +1,20 @@
 from django.db import models
 
 
+class Location(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=300)
+
+    def __str__(self):
+        return "{} ({})".format(self.name, self.address)
+
+
 class Meetup(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField(unique=True, allow_unicode=True)
     description = models.TextField()
     image = models.ImageField(upload_to="images")  # /uploads/images
-    # TODO: add location
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
