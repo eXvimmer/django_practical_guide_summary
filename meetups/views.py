e23474a71b409dc3182509f6be057af37c455eb6
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-08-17 11:15:10 +0430

    Interpolation and tags

--- meetups/views.py
+++ meetups/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpRequest
 from django.shortcuts import render
 
+meetups = [{"title": "First Meetup"}, {"title": "Second Meetup"}]
+
 
 def index(request: HttpRequest):
-    return render(request, "meetups/index.html")
+    return render(
+        request, "meetups/index.html", {"meetups": meetups, "show_meetups": True}
+    )
